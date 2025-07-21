809322f6321fd766124db8b68e08c07de491adea
sp7567 <shrayaspadase7567@gmail.com>
2025-07-22 00:00:13 +0530

    Your commit message

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+require('dotenv').config(); // First line
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("✅ MongoDB Connected"))
+.catch(err => console.error("❌ MongoDB connection error:", err));
